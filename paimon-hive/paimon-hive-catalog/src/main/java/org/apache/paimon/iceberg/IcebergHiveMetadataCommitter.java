13340b601b9f357fda79b2fa1857e4574a2d2b23
zhengsheng.du <zhengsheng.du@msxf.com>
2025-09-20 15:09:53 +0800

    Change constant reference for DO_NOT_UPDATE_STATS in Hive table.
    Replaced the hardcoded string with `StatsSetupConst.DO_NOT_UPDATE_STATS` to ensure consistency and reduce potential errors. This improves maintainability and aligns with best practices for constant usage.

--- paimon-hive/paimon-hive-catalog/src/main/java/org/apache/paimon/iceberg/IcebergHiveMetadataCommitter.java
+++ paimon-hive/paimon-hive-catalog/src/main/java/org/apache/paimon/iceberg/IcebergHiveMetadataCommitter.java
@@ -215,7 +215,9 @@ public class IcebergHiveMetadataCommitter implements IcebergMetadataCommitter {
         Options options = new Options(table.options());
         boolean skipHiveUpdateStats = options.get(IcebergOptions.HIVE_SKIP_UPDATE_STATS);
 
-        hiveTable.getParameters().put("DO_NOT_UPDATE_STATS", Boolean.toString(skipHiveUpdateStats));
+        hiveTable
+                .getParameters()
+                .put(StatsSetupConst.DO_NOT_UPDATE_STATS, Boolean.toString(skipHiveUpdateStats));
         hiveTable.getParameters().put("EXTERNAL", "TRUE");
         hiveTable.getParameters().put("table_type", "ICEBERG");
 
